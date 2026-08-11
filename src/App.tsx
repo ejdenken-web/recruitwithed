430b5ed283ded6aa78c2bb722c7d19a28f7b4f18
ejdenken-web <ej.denken@gmail.com>
2026-08-11 11:22:21 +0000

    Remove redundant technology stack section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import "./App.css";
-
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import AboutImpact from "./components/AboutImpact";
 import Clients from "./components/Clients";
 import Certifications from "./components/Certifications";
 import Projects from "./components/Projects";
-import TechStack from "./components/TechStack";
 import Footer from "./components/Footer";
 
 function App() {
@@ -20,15 +16,11 @@ function App() {
 
         <About />
 
-        <AboutImpact />
-
         <Clients />
 
         <Certifications />
 
         <Projects />
-
-        <TechStack />
       </main>
 
       <Footer />
