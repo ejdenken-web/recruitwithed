5e375e7c52f637e5b838c727905d11a7f1c41403
FairyMinneapolis <ej.denken@gmail.com>
2026-08-27 20:53:56 -0400

    Unify site colors and theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import "./styles/theme.css";
 import "./index.css";
 import "./App.css";
+import "./styles/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
